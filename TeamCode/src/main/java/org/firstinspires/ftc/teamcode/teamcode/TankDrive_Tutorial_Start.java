1c3f3c123bda1a57465733107427fbb6ef132b7b
IndecisiveDevices <ShamRobotics@outlook.com>
2026-06-01 01:35:43 -0400

    Update tank drive tutorial to reflect correct page counts and improve clarity in code examples

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/TankDrive_Tutorial_Start.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/TankDrive_Tutorial_Start.java
@@ -5,12 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name = "Ed - Tank Drive Example", group = "Workshop")
+@TeleOp(name = "{your name} - Tank Drive", group = "Workshop")
 public class TankDrive_Tutorial_Start extends OpMode {
     private DcMotor frontLeftDrive;
-    private DcMotor frontRightDrive;
-    private DcMotor rearLeftDrive;
-    private DcMotor rearRightDrive;
 
     @Override
     public void init() {
